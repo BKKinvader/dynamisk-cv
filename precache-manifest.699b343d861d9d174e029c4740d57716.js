c26599fc6b20aca16070f3f228e89e3afc354fe4
BKKinvader <tim.nilsson97@hotmail.com>
2023-03-30 12:40:18 +0200

    Updates

---
+++ precache-manifest.699b343d861d9d174e029c4740d57716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16f532bc4b91a22742fc",
+    "url": "/dynamisk-cv/static/css/main.103019d5.chunk.css"
+  },
+  {
+    "revision": "16f532bc4b91a22742fc",
+    "url": "/dynamisk-cv/static/js/main.16f532bc.chunk.js"
+  },
+  {
+    "revision": "a6b499e470baee4f6a24",
+    "url": "/dynamisk-cv/static/js/1.a6b499e4.chunk.js"
+  },
+  {
+    "revision": "0a0155e40ffff3721765",
+    "url": "/dynamisk-cv/static/js/runtime~main.0a0155e4.js"
+  },
+  {
+    "revision": "56b3faeb775aa890c276407ae1d47c0d",
+    "url": "/dynamisk-cv/index.html"
+  }
+];
